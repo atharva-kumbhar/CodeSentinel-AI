26e1861a07c6d0b2abcdf04d793f10785b48f580
Atharva Rajendra Kumbhar <AtharvaRK07@users.noreply.huggingface.co>
2026-05-24 23:36:14 +0000

    fixing the port no

--- backend/main.py
+++ backend/main.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
+        port=int(os.getenv("PORT", 7860)),
         reload=os.getenv("ENV", "development") == "development",
         log_level="info",
     )
